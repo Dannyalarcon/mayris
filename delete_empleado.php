e6edc62a34ddfdb291e5f51f2a617dfd63d87282
Danny_Alarcon_7 <dannyalarcon417@gmail.com>
2020-10-18 11:52:45 -0600

    dar de baja a empleado
    dar de baja a empleado

---
+++ delete_empleado.php
@@ -0,0 +1,22 @@
+<?php
+//se llama la conexion a la base de datos
+include 'conexion.php';
+$conection = @mysqli_connect($host, $user, $password, $db);
+if ($_SESSION['rol'] != 1) {
+    header('location: principal.php');
+}
+
+//se llama el id seleccionado
+$id_usuario = $_GET['id'];
+
+//se hace una consulta delete para el id
+$query_delete = mysqli_query($conection, "UPDATE usuario SET user='0', pass='0', estatus = '0' WHERE id_usuario=$id_usuario");
+
+//si todo esta correcto se elimina el id y se redirige a la vista principal
+if($query_delete){
+    header('location: empleado.php');
+    echo 'Se elimino correctamente';
+}else{
+echo 'No nada';
+}
+/*se cierra la conexion*/
